b5bf412ddc98b6381cd1b9aa209b17b8b56aefe5
mgd <mr.zizikk@gamil.com>
2023-07-22 13:10:23 -0400

    create submenu schemas

---
+++ src/db/schemas/submenu.py
@@ -0,0 +1,28 @@
+import uuid
+from typing import Optional
+
+from pydantic import BaseModel
+
+
+class SubmenuCreate(BaseModel):
+    """Create submenu schema."""
+    title: str
+    description: str
+
+
+class SubmenuUpdate(BaseModel):
+    """Update submenu schema."""
+    title: Optional[str]
+    description: Optional[str]
+
+
+class SubmenuResponse(BaseModel):
+    """Response submenu schema."""
+    id: uuid.UUID
+    title: str
+    description: str
+    menu_id: uuid.UUID
+    quantity_dish: int = 0
+
+    class Config:
+        from_attributes = True
