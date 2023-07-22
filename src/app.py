f653f96ef9c44318ee9221ab95ada18d8ffafd1a
mgd <mr.zizikk@gamil.com>
2023-07-22 13:19:26 -0400

    include submenu router to main router and add params to routers

--- src/app.py
+++ src/app.py
@@ -4,13 +4,27 @@ from fastapi import (
 )
 
 from src.endpoints.menu import menu_route
+from src.endpoints.submenu import submenu_route
 
 
-app = FastAPI()
+app = FastAPI(
+    title='Restaurant menu API',
+    description='FastApi project',
+    version='0.1.1'
+)
 
 main_route = APIRouter(
     prefix='/api/v1'
 )
 
-main_route.include_router(menu_route)
+main_route.include_router(
+    menu_route,
+    prefix='/menus',
+    tags=['menus'],
+)
+main_route.include_router(
+    submenu_route,
+    prefix='/menus/{menu_id}/submenus',
+    tags=['submenu'],
+)
 app.include_router(main_route)
