39866cb3b015887a226082f79846c3b94ee0e37b
mgd <mr.zizikk@gamil.com>
2023-07-22 10:15:20 -0400

    import app and define middleware

---
+++ main.py
@@ -0,0 +1,23 @@
+import uvicorn
+from fastapi import (
+    Request,
+    Response,
+)
+
+from src.app import app
+from src.db.core import session
+
+
+@app.middleware('http')
+async def db_session_middleware(request: Request, call_next):
+    response = Response('Internal server error', status_code=500)
+    try:
+        request.state.db = session
+        response = await call_next(request)
+    finally:
+        await request.state.db.close()
+    return response
+
+
+if __name__ == '__main__':
+    uvicorn.run(app=app)
