b2df2f29aa1519d63576ba61131736d45ccc91e9
mgd <mr.zizikk@gamil.com>
2023-07-24 09:18:31 -0400

    create test configs

--- src/config.py
+++ src/config.py
@@ -5,6 +5,7 @@ from dotenv import load_dotenv
 
 __all__ = (
     'settings',
+    'test_settings',
 )
 
 load_dotenv()
@@ -21,4 +22,16 @@ class BaseConfig:
                     f'{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}')
 
 
+class TestConfig:
+    """Test config."""
+    DB_USER = os.getenv('DB_USER', 'test')
+    DB_PASSWORD = os.getenv('DB_PASSWORD', 'test')
+    DB_HOST = os.getenv('TEST_DB_HOST', 'test_db')
+    DB_NAME = os.getenv('TEST_DB_NAME', 'test_menu_db')
+
+    DATABASE_URL = (f'postgresql+asyncpg://'
+                    f'{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}')
+
+
 settings = BaseConfig()
+test_settings = TestConfig()
