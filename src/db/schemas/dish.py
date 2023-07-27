6b1f1418df935f7006cb8fd51cb28f7586d4ea07
mgd <mr.zizikk@gamil.com>
2023-07-27 07:42:29 -0400

    refactoring validation of fields

--- src/db/schemas/dish.py
+++ src/db/schemas/dish.py
@@ -2,21 +2,24 @@ import decimal
 import uuid
 from typing import Optional
 
-from pydantic import BaseModel
+from pydantic import (
+    BaseModel,
+    Field,
+)
 
 
 class DishCreate(BaseModel):
     """Create dish schema."""
-    title: str
+    title: str = Field(min_length=3)
     description: str
-    price: decimal.Decimal
+    price: decimal.Decimal = Field(ge=0)
 
 
 class DishUpdate(BaseModel):
     """Update dish schema."""
-    title: Optional[str]
+    title: Optional[str] = Field(min_length=3)
     description: Optional[str]
-    price: Optional[decimal.Decimal]
+    price: Optional[decimal.Decimal] = Field(ge=0)
 
 
 class DishResponse(BaseModel):
