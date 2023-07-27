ae064e22ac66b769a5fc7f8e69f3509dcbcd4d13
mgd <mr.zizikk@gamil.com>
2023-07-27 08:14:17 -0400

    refactoring all tests for menu api and add docs

--- tests/menu_tests/menu_api_tests.py
+++ tests/menu_tests/menu_api_tests.py
@@ -1,163 +1,173 @@
-from fastapi import FastAPI
-from httpx import AsyncClient
+import uuid
+from typing import (
+    Dict,
+    Union,
+)
+
+from httpx import Response
 
 
 class TestMenuAPI:
 
-    async def get_url(self, name, app, **kwargs):
-        return app.url_path_for(
-            name,
-            **kwargs
-        )
-
-    async def detail_menu_url(self, client, app, name):
-        menus_url = await self.get_url('get_list_menu', app=app)
-        menus = await client.get(menus_url, follow_redirects=True)
-
-        menu_id = menus.json()[0].get('id')
-
-        return await self.get_url(
-            name,
-            app=app,
-            menu_id=menu_id
-        )
-
     async def test_get_menu_empty_list(
             self,
-            client: AsyncClient,
-            get_app: FastAPI
+            menus_list_response: Response
     ):
-        url = await self.get_url(
-            name='get_list_menu',
-            app=get_app
+        """Test for getting an empty list of menus."""
+
+        assert (
+                menus_list_response.headers.get('content-type') ==
+                'application/json'
         )
+        assert menus_list_response.status_code == 200
+        assert menus_list_response.json() == []
 
-        response = await client.get(url, follow_redirects=True)
-
-        assert response.status_code == 200
-        assert response.json() == []
-
-    async def test_menu_create(
+    async def test_menu_create_successful(
             self,
-            client: AsyncClient,
-            get_app: FastAPI,
-            create_menu_data: dict
+            create_menu_response: Response,
+            response_menu_data: Dict[str, Union[str, int, uuid.UUID]],
+            create_menu_data: Dict[str, str]
     ):
-        url = await self.get_url(
-            app=get_app,
-            name='create_menu'
-        )
+        """Test the successful creation of the menu."""
 
-        response = await client.post(
-            url=url,
-            json=create_menu_data,
-            follow_redirects=True
+        assert create_menu_response.status_code == 201
+        assert isinstance(create_menu_response.json(), dict)
+        assert (
+                create_menu_response.headers.get('content-type') ==
+                'application/json'
         )
-
-        assert response.status_code == 201
-        assert response.headers.get('content-type') == 'application/json'
-        assert isinstance(response.json(), dict)
-        assert isinstance(response.json().get('id'), str)
-        assert response.json().get('title') == 'Menu 1'
-        assert response.json().get('description') == 'Menu description 1'
+        assert (
+                create_menu_response.json().get('id') ==
+                response_menu_data.get('id')
+        )
+        assert (
+                create_menu_response.json().get('title') ==
+                create_menu_data.get('title')
+        )
+        assert (
+                create_menu_response.json().get('description') ==
+                create_menu_data.get('description')
+        )
+        assert create_menu_response.json() == response_menu_data
 
     async def test_get_menu_list_not_empty(
             self,
-            client: AsyncClient,
-            get_app: FastAPI,
+            menus_list_response: Response,
+            response_menu_data: Dict[str, Union[str, int, uuid.UUID]]
     ):
-        url = await self.get_url(
-            app=get_app,
-            name='get_list_menu'
-        )
+        """Test for getting a list of menus after creation."""
 
-        response = await client.get(url, follow_redirects=True)
+        assert menus_list_response.status_code == 200
+        assert len(menus_list_response.json()) == 1
+        assert isinstance(menus_list_response.json(), list)
+        assert menus_list_response.json() == [response_menu_data]
 
-        assert response.status_code == 200
-        assert len(response.json()) == 1
-        assert isinstance(response.json(), list)
-
-    async def test_get_menu_detail(
+    async def test_get_menu_detail_successful(
             self,
-            client: AsyncClient,
-            get_app: FastAPI
+            detail_menu_response: Response,
+            response_menu_data: Dict[str, Union[str, int, uuid.UUID]],
+            create_menu_data: Dict[str, str]
     ):
-        url = await self.detail_menu_url(
-            name='get_detail_menu',
-            client=client,
-            app=get_app
+        """Test for getting a detail of menu."""
+
+        assert detail_menu_response.status_code == 200
+        assert (
+                detail_menu_response.headers.get('content-type') ==
+                'application/json'
+        )
+        assert isinstance(detail_menu_response.json(), dict)
+        assert (
+                detail_menu_response.json().get('id') ==
+                response_menu_data.get('id')
+        )
+        assert (
+                detail_menu_response.json().get('title') ==
+                create_menu_data.get('title')
+        )
+        assert (
+                detail_menu_response.json().get('description') ==
+                create_menu_data.get('description')
         )
 
-        response = await client.get(url, follow_redirects=True)
-
-        assert response.status_code == 200
-        assert response.headers.get('content-type') == 'application/json'
-        assert len(response.json()) > 0
-        assert isinstance(response.json(), dict)
-        assert isinstance(response.json().get('id'), str)
-        assert response.json().get('title') == 'Menu 1'
-        assert response.json().get('description') == 'Menu description 1'
-
-    async def test_update_menu_by_id(
+    async def test_update_detail_of_menu_successful(
             self,
-            client: AsyncClient,
-            get_app: FastAPI,
-            update_menu_data: dict
+            update_menu_response: Response,
+            response_menu_data: Dict[str, Union[str, int, uuid.UUID]],
+            update_menu_data: Dict[str, str]
     ):
-        url = await self.detail_menu_url(
-            name='update_menu',
-            client=client,
-            app=get_app
+        """Test for successful updating a detail of menu."""
+
+        assert update_menu_response.status_code == 200
+        assert isinstance(update_menu_response.json(), dict)
+        assert (
+                update_menu_response.headers.get('content-type') ==
+                'application/json'
+        )
+        assert (
+                update_menu_response.json().get('id') ==
+                response_menu_data.get('id')
+        )
+        assert (
+                update_menu_response.json().get('title') ==
+                update_menu_data.get('title')
+        )
+        assert (
+                update_menu_response.json().get('description') ==
+                update_menu_data.get('description')
         )
 
-        response = await client.patch(
-            url,
-            json=update_menu_data,
-            follow_redirects=True
-        )
-
-        assert response.status_code == 200
-        assert response.headers.get('content-type') == 'application/json'
-        assert len(response.json()) > 0
-        assert isinstance(response.json(), dict)
-        assert isinstance(response.json().get('id'), str)
-        assert response.json().get('title') == 'Updated menu'
-        assert response.json().get('description') == 'Updated menu description'
-
-    async def test_delete_menu(
+    async def test_get_menu_detail_after_update(
             self,
-            client: AsyncClient,
-            get_app: FastAPI
+            detail_menu_response: Response,
+            response_menu_data: Dict[str, Union[str, int, uuid.UUID]],
+            update_menu_data: Dict[str, str]
     ):
-        url = await self.detail_menu_url(
-            name='delete_menu',
-            client=client,
-            app=get_app,
+        """Test to check data after update a details of menu."""
+
+        assert detail_menu_response.status_code == 200
+        assert isinstance(detail_menu_response.json(), dict)
+        assert (
+                detail_menu_response.headers.get('content-type') ==
+                'application/json'
+        )
+        assert (
+                detail_menu_response.json().get('id') ==
+                response_menu_data.get('id')
+        )
+        assert (
+                detail_menu_response.json().get('title') ==
+                update_menu_data.get('title')
+        )
+        assert (
+                detail_menu_response.json().get('description') ==
+                update_menu_data.get('description')
         )
 
-        response = await client.delete(
-            url,
-            follow_redirects=True
-        )
-
-        assert response.status_code == 200
-        assert response.headers.get('content-type') == 'application/json'
-
-    async def test_get_menu_by_id_empty(
+    async def test_delete_menu_successful(
             self,
-            client: AsyncClient,
-            get_app: FastAPI
+            delete_menu_response: Response
     ):
-        url = await self.get_url(
-            'get_detail_menu',
-            app=get_app,
-            menu_id='5372d4ba-1e98-4b37-b1fe-000000000000'
+        """Test to check the deletion of a menu."""
+
+        assert delete_menu_response.status_code == 200
+        assert (
+                delete_menu_response.headers.get('content-type') ==
+                'application/json'
         )
 
-        response = await client.get(
-            url,
-            follow_redirects=True
-        )
+    async def test_get_menu_empty_list_after_delete(
+            self,
+            menus_list_response: Response
+    ):
+        """Test to check menus after deletion of a menu."""
 
-        assert response.json().get('detail') == 'menu not found'
-        assert response.status_code == 404
+        await self.test_get_menu_empty_list(menus_list_response)
+
+    async def test_get_non_existent_menu(
+            self,
+            non_existent_menu_response: Response
+    ):
+        """Test to check a non-existent menu."""
+
+        assert non_existent_menu_response.json().get('detail') == 'menu not found'
+        assert non_existent_menu_response.status_code == 404
