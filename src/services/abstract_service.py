d919548de4f8af60b30ddc8d2321119aa7b42be6
mgd <mr.zizikk@gamil.com>
2023-08-05 16:57:04 -0400

    refactoring abstract service class, add methods for work with cache

--- src/services/abstract_service.py
+++ src/services/abstract_service.py
@@ -1,11 +1,22 @@
-from abc import (
-    ABC,
-    abstractmethod,
-)
+from abc import ABC, abstractmethod
+
+from src.cache.abstract_cache import AbstractCache
+from src.repositories.abstract_repository import AbstractRepository
 
 
 class AbstractService(ABC):
     """Abstract service class."""
+    repo: AbstractRepository
+    cache: AbstractCache
+
+    detail_menu = 'menu:{}'
+    menus_list = 'list_of_menus'
+
+    detail_submenu = 'menu:{0}:submenu:{1}'
+    submenus_list = 'menu:{}:list_of_submenus'
+
+    detail_dish = 'menu:{0}:submenu:{1}:dish:{2}'
+    dishes_list = 'menu:{0}:submenu:{1}:list_of_dishes'
 
     @abstractmethod
     async def get_detail(self, *args, **kwargs):
@@ -31,3 +42,23 @@ class AbstractService(ABC):
     async def delete(self, *args, **kwargs):
         """Abstract delete method require for implementation."""
         pass
+
+    async def get_obj_from_cache(self, key: str):
+        try:
+            obj = await self.cache.get(self.detail_menu.format(key))
+        except Exception:
+            obj = None
+
+        return obj
+
+    async def set_value_into_cache(self, key, value):
+        try:
+            await self.cache.set(key, value)
+        except Exception:
+            pass
+
+    async def cache_invalidate(self, *args, **kwargs):
+        try:
+            await self.cache.delete(args, **kwargs)
+        except Exception:
+            pass
