d9e2a842ca6ddfc3d28561c057ac5874a4f25a26
mgd <mr.zizikk@gamil.com>
2023-08-05 16:58:32 -0400

    refactoring menu service class, add cache

--- src/services/menu.py
+++ src/services/menu.py
@@ -1,121 +1,144 @@
 import uuid
-from typing import (
-    Optional,
-    Sequence,
-)
 
-from fastapi import (
-    Depends,
-    HTTPException,
-    status,
-)
-from sqlalchemy import Row
+from fastapi import Depends, HTTPException, status
 
+from src.cache.cache import Cache, get_cache
 from src.db import models
-from src.db.crud.menu import (
-    MenuCRUD,
-    get_menu_crud,
-)
 from src.db.schemas import Status
-from src.db.schemas.menu import (
-    MenuCreate,
-    MenuUpdate,
-)
+from src.db.schemas.menu import MenuCreate, MenuUpdate
+from src.repositories.menu import MenuRepository, get_menu_repo
 from src.services.abstract_service import AbstractService
 
 
 class MenuService(AbstractService):
     """Service layer."""
 
-    def __init__(self, crud: MenuCRUD):
-        self.crud = crud
+    def __init__(self, repo: MenuRepository, cache: Cache):
+        self.repo = repo
+        self.cache = cache
 
-    async def get_detail(self, menu_id: uuid.UUID) -> Optional[models.Menu]:
-        """Get detail of menu from DB layer."""
-        try:
-            menu = await self.crud.get_detail(menu_id)
-        except Exception as error:
-            raise HTTPException(
-                status_code=status.HTTP_400_BAD_REQUEST,
-                detail=error.args[0]
-            )
-        else:
-            if not menu:
+    async def get_detail(
+            self,
+            menu_id: uuid.UUID
+    ) -> dict:
+        """Get detail of menu from cache or DB layer."""
+        menu = await self.get_obj_from_cache(self.detail_menu.format(menu_id))
+
+        if not menu:
+            try:
+                menu = await self.repo.get_detail(menu_id)
+            except Exception as error:
                 raise HTTPException(
-                    status_code=status.HTTP_404_NOT_FOUND,
-                    detail='menu not found'
+                    status_code=status.HTTP_400_BAD_REQUEST,
+                    detail=error.args
                 )
+
+        if not menu:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail='menu not found'
+            )
+
+        await self.set_value_into_cache(
+            self.detail_menu.format(menu_id),
+            menu
+        )
 
         return menu
 
-    async def get_list(self) -> Sequence[Row]:
-        """Get list of menus from DB layer."""
-        try:
-            menus = await self.crud.get_list()
-        except Exception as error:
-            raise HTTPException(
-                status_code=status.HTTP_400_BAD_REQUEST,
-                detail=error.args[0]
-            )
+    async def get_list(self) -> list:
+        """Get list of menus from cache or DB layer."""
+        menus = await self.get_obj_from_cache(self.menus_list)
+
+        if not menus:
+            try:
+                menus = await self.repo.get_list()
+            except Exception as error:
+                raise HTTPException(
+                    status_code=status.HTTP_400_BAD_REQUEST,
+                    detail=error.args
+                )
+            if menus:
+                await self.set_value_into_cache(
+                    self.menus_list,
+                    menus
+                )
 
         return menus
 
-    async def create(self, data: MenuCreate) -> Optional[models.Menu]:
-        """Create new menu logic."""
+    async def create(self, data: MenuCreate) -> models.Menu | None:
+        """Create new menu logic and remove menus from cache."""
         try:
-            async with self.crud.session.begin():
-                new_menu = await self.crud.create(data)
+            async with self.repo.session.begin():
+                new_menu = await self.repo.create(data)
+
         except Exception as error:
             raise HTTPException(
                 status_code=status.HTTP_400_BAD_REQUEST,
-                detail=error.args[0]
+                detail=error.args
             )
+
+        await self.cache_invalidate(self.menus_list)
 
         return new_menu
 
-    async def update(self,
-                     menu_id: uuid.UUID,
-                     data: MenuUpdate) -> Optional[models.Menu]:
-        """Update menu logic."""
+    async def update(
+            self,
+            menu_id: uuid.UUID,
+            data: MenuUpdate
+    ) -> models.Menu | None:
+        """Update menu logic and remove this menu and menus from cache."""
         try:
-            async with self.crud.session.begin():
-                menu = await self.crud.update(menu_id=menu_id, data=data)
+            async with self.repo.session.begin():
+                menu = await self.repo.update(menu_id=menu_id, data=data)
+
         except Exception as error:
             raise HTTPException(
                 status_code=status.HTTP_400_BAD_REQUEST,
-                detail=error.args[0]
+                detail=error.args
             )
-        else:
-            if not menu:
-                raise HTTPException(
-                    status_code=status.HTTP_404_NOT_FOUND,
-                    detail='menu not found'
-                )
+
+        if not menu:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail='menu not found'
+            )
+        await self.cache_invalidate(
+            self.menus_list,
+            self.detail_menu.format(menu_id),
+        )
 
         return menu
 
     async def delete(self, menu_id: uuid.UUID) -> Status:
-        """Delete menu logic."""
+        """Delete menu logic and remove this menu and menus form cache."""
         try:
-            async with self.crud.session.begin():
-                is_deleted = await self.crud.delete(menu_id)
+            async with self.repo.session.begin():
+                is_deleted = await self.repo.delete(menu_id)
         except Exception as error:
             raise HTTPException(
                 status_code=status.HTTP_400_BAD_REQUEST,
-                detail=error.args[0]
+                detail=error.args
             )
-        else:
-            if not is_deleted:
-                raise HTTPException(
-                    status_code=status.HTTP_404_NOT_FOUND,
-                    detail='menu not found'
-                )
+
+        if not is_deleted:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail='menu not found'
+            )
+
+        await self.cache_invalidate(
+            self.menus_list,
+            self.detail_menu.format(menu_id),
+            invalid_key=menu_id
+        )
 
         return Status(message=f'Menu {menu_id} was successfully deleted!')
 
 
 async def get_menu_service(
-        crud: MenuCRUD = Depends(get_menu_crud)
+        repo: MenuRepository = Depends(get_menu_repo),
+        cache: Cache = Depends(get_cache)
 ) -> MenuService:
     """Instance of MenuService."""
-    return MenuService(crud=crud)
+    return MenuService(repo=repo, cache=cache)
