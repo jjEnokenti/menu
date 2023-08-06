865c72c46890209b75173b5d6daee54a9f721b04
mgd <mr.zizikk@gamil.com>
2023-08-06 04:20:59 -0400

    correcting the style of arguments

--- src/endpoints/submenu.py
+++ src/endpoints/submenu.py
@@ -9,11 +9,13 @@ from src.services.submenu import SubmenuService, get_submenu_service
 submenu_route = APIRouter()
 
 
-@submenu_route.get('/submenus',
-                   summary='Get list',
-                   description='Get list of submenus',
-                   response_model=list[SubmenuResponse],
-                   status_code=status.HTTP_200_OK)
+@submenu_route.get(
+    '/submenus',
+    summary='Get list',
+    description='Get list of submenus',
+    response_model=list[SubmenuResponse],
+    status_code=status.HTTP_200_OK
+)
 async def get_list_submenu(
         menu_id: uuid.UUID,
         submenu_service: SubmenuService = Depends(get_submenu_service)
@@ -21,22 +23,28 @@ async def get_list_submenu(
     return await submenu_service.get_list(menu_id)
 
 
-@submenu_route.post('/submenus',
-                    summary='Create submenu',
-                    description='Create new submenu',
-                    response_model=SubmenuResponse,
-                    status_code=status.HTTP_201_CREATED)
-async def create_submenu(menu_id: uuid.UUID,
-                         data: SubmenuCreate,
-                         submenu_service: SubmenuService = Depends(get_submenu_service)):
+@submenu_route.post(
+    '/submenus',
+    summary='Create submenu',
+    description='Create new submenu',
+    response_model=SubmenuResponse,
+    status_code=status.HTTP_201_CREATED
+)
+async def create_submenu(
+        menu_id: uuid.UUID,
+        data: SubmenuCreate,
+        submenu_service: SubmenuService = Depends(get_submenu_service)
+):
     return await submenu_service.create(menu_id=menu_id, data=data)
 
 
-@submenu_route.get('/submenus/{submenu_id}',
-                   summary='get detail',
-                   description='Get detail of submenu by submenu_id',
-                   response_model=SubmenuResponse,
-                   status_code=status.HTTP_200_OK)
+@submenu_route.get(
+    '/submenus/{submenu_id}',
+    summary='get detail',
+    description='Get detail of submenu by submenu_id',
+    response_model=SubmenuResponse,
+    status_code=status.HTTP_200_OK
+)
 async def get_detail_submenu(
         menu_id: uuid.UUID,
         submenu_id: uuid.UUID,
@@ -45,24 +53,32 @@ async def get_detail_submenu(
     return await submenu_service.get_detail(menu_id=menu_id, submenu_id=submenu_id)
 
 
-@submenu_route.patch('/submenus/{submenu_id}',
-                     summary='Update submenu',
-                     description='Update submenu by submenu_id',
-                     response_model=SubmenuResponse,
-                     status_code=status.HTTP_200_OK)
-async def update_submenu(menu_id: uuid.UUID,
-                         submenu_id: uuid.UUID,
-                         data: SubmenuUpdate,
-                         submenu_service: SubmenuService = Depends(get_submenu_service)):
+@submenu_route.patch(
+    '/submenus/{submenu_id}',
+    summary='Update submenu',
+    description='Update submenu by submenu_id',
+    response_model=SubmenuResponse,
+    status_code=status.HTTP_200_OK
+)
+async def update_submenu(
+        menu_id: uuid.UUID,
+        submenu_id: uuid.UUID,
+        data: SubmenuUpdate,
+        submenu_service: SubmenuService = Depends(get_submenu_service)
+):
     return await submenu_service.update(menu_id=menu_id, submenu_id=submenu_id, data=data)
 
 
-@submenu_route.delete('/submenus/{submenu_id}',
-                      summary='Delete submenu',
-                      description='Delete submenu by submenu_id',
-                      response_model=Status,
-                      status_code=status.HTTP_200_OK)
-async def delete_submenu(menu_id: uuid.UUID,
-                         submenu_id: uuid.UUID,
-                         submenu_service: SubmenuService = Depends(get_submenu_service)):
+@submenu_route.delete(
+    '/submenus/{submenu_id}',
+    summary='Delete submenu',
+    description='Delete submenu by submenu_id',
+    response_model=Status,
+    status_code=status.HTTP_200_OK
+)
+async def delete_submenu(
+        menu_id: uuid.UUID,
+        submenu_id: uuid.UUID,
+        submenu_service: SubmenuService = Depends(get_submenu_service)
+):
     return await submenu_service.delete(menu_id=menu_id, submenu_id=submenu_id)
