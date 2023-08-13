aa91c7db08bc5bfcfbeb8f5c4d1d0ff11e2117a0
mgd <mr.zizikk@gamil.com>
2023-08-13 13:57:23 -0400

    add keys for cache invalidation

---
+++ src/api/keys_for_cache_invalidation.py
@@ -0,0 +1,10 @@
+ALL_DATA = 'all_data_for_db'
+
+DETAIL_MENU = 'menu:{}'
+MENUS_LIST = 'list_of_menus'
+
+DETAIL_SUBMENU = 'menu:{0}:submenu:{1}'
+SUBMENUS_LIST = 'menu:{}:list_of_submenus'
+
+DETAIL_DISH = 'menu:{0}:submenu:{1}:dish:{2}'
+DISHES_LIST = 'menu:{0}:submenu:{1}:list_of_dishes'
