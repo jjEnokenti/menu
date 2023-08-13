71494f35459aa7858b3aabc038c7ff5cce2caca3
mgd <mr.zizikk@gamil.com>
2023-08-13 13:29:59 -0400

    refactoring variables, add rabbitmq variables, base dir path and Menu.xlsx path

--- src/config.py
+++ src/config.py
@@ -1,3 +1,5 @@
+from pathlib import Path
+
 from dotenv import load_dotenv
 from pydantic_settings import BaseSettings
 
@@ -12,22 +14,41 @@ class Settings(BaseSettings):
     """App settings."""
     MODE: str
 
+    BASE_DIR: str = Path(__file__).resolve().parent.parent.as_posix()
+    ADMIN_FILE_PATH: str = f'{BASE_DIR}/admin/Menu.xlsx'
+
     DB_USER: str
     DB_PASSWORD: str
-    DB_HOST: str
     DB_NAME: str
-    DB_PORT: int
+    DB_HOST: str
+    DB_PORT: int = 5432
 
-    REDIS_HOST: str
-    REDIS_PORT: int
-    REDIS_DB: int
     REDIS_CACHE_EXPIRE: int
+    REDIS_DB: int
+    REDIS_HOST: str
+    REDIS_PORT: int = 6379
+
+    RABBITMQ_USER: str
+    RABBITMQ_PASSWORD: str
+    RABBITMQ_HOST: str
+    RABBITMQ_PORT: int = 5672
+    CELERY_RESULT_BACKEND: str = 'rpc://'
+
+    @property
+    def BROKER_URL(self) -> str:
+        """Return broker url."""
+        return (
+            f'amqp://{self.RABBITMQ_USER}:{self.RABBITMQ_PASSWORD}@'
+            f'{self.RABBITMQ_HOST}:{self.RABBITMQ_PORT}//'
+        )
 
     @property
     def DATABASE_URL(self) -> str:
         """Return db url."""
-        return (f'postgresql+asyncpg://'
-                f'{self.DB_USER}:{self.DB_PASSWORD}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}')
+        return (
+            f'postgresql+asyncpg://'
+            f'{self.DB_USER}:{self.DB_PASSWORD}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}'
+        )
 
     class ConfigDict:
         env_files = '.env'
