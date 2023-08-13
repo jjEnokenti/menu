3428ee03c543a1bd0451c9ede50468b013091a7c
mgd <mr.zizikk@gamil.com>
2023-08-13 13:33:23 -0400

    rename function get redis instance, refactoring db param

--- src/cache/core.py
+++ src/cache/core.py
@@ -3,10 +3,10 @@ from aioredis import Redis
 from src.config import settings
 
 
-async def get_redis_cache() -> Redis:
+async def get_redis_instance() -> Redis:
     """Redis cache configurate instance."""
     return Redis(
         host=settings.REDIS_HOST,
         port=settings.REDIS_PORT,
-        db=settings.REDIS_DB
+        db=settings.REDIS_DB,
     )
