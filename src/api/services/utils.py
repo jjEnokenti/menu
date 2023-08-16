13282ec284e351b35ebb4f963dc042e7ebf9faf0
mgd <mr.zizikk@gamil.com>
2023-08-16 06:46:01 -0400

    refactoring discount check conition

--- src/api/services/utils.py
+++ src/api/services/utils.py
@@ -8,7 +8,7 @@ async def set_discounts(discounts: dict, dishes: list) -> list:
     for i in range(len(dishes) - 1, -1, -1):
         dish = dishes[i]
         discount = discounts.get(f'discount:{dish["id"]}')
-        if discount:
+        if discount is not None:
             del dishes[i]
             dish['price'] = discount
             dishes.append(dish)
